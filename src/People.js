2ee7c3892085f8da0f2e0601aa046f73a69110f1
christopherstraub <ccstraub@gmail.com>
2020-04-06 15:22:51 -0500

    successfully changed from pure function to class

--- src/People.js
+++ src/People.js
@@ -2,34 +2,66 @@ import React from 'react';
 
 const getHomeWorldUrl = (visibleResourceData, visibleIndex) => {
   const homeWorldUrl = visibleResourceData[0][visibleIndex].homeworld;
-  console.log(homeWorldUrl);
   return homeWorldUrl;
 };
 
 const fetchHomeWorldData = (url) => {
   return fetch(url)
     .then((response) => response.json())
-    .then((data) => data.name);
+    .then((data) => data);
 };
 
-const People = ({ number, visibleIndex, visibleResourceData }) => {
-  return (
-    <div>
-      <h1>{visibleResourceData[0][visibleIndex].name}</h1>
-      <h1>{visibleResourceData[0][visibleIndex].height / 100}m tall</h1>
-      <h1>{visibleResourceData[0][visibleIndex].mass}kg</h1>
-      <h1>Hair {visibleResourceData[0][visibleIndex].hair_color}</h1>
-      <h1>{visibleResourceData[0][visibleIndex].skin_color} skin</h1>
-      <h1>Born {visibleResourceData[0][visibleIndex].birth_year}</h1>
-      <h1>Gender {visibleResourceData[0][visibleIndex].gender}</h1>
-      <h4>{number}</h4>
-      {console.log(
-        fetchHomeWorldData(
-          getHomeWorldUrl(visibleResourceData, visibleIndex)
-        ).then(console.log)
-      )}
-    </div>
-  );
+const getSpeciesUrl = (visibleResourceData, visibleIndex) => {
+  const speciesUrl = visibleResourceData[0][visibleIndex].species[0];
+  return speciesUrl;
 };
 
+const fetchSpeciesData = (url) => {
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data);
+};
+
+class People extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      homeWorld: null,
+      species: null,
+    };
+  }
+
+  componentDidMount() {
+    fetchHomeWorldData(
+      getHomeWorldUrl(this.props.visibleResourceData, this.props.visibleIndex)
+    ).then((data) => {
+      this.setState({ homeWorld: data.name });
+    });
+
+    fetchSpeciesData(
+      getSpeciesUrl(this.props.visibleResourceData, this.props.visibleIndex)
+    ).then((data) => {
+      this.setState({ species: data.name });
+    });
+  }
+
+  render() {
+    const { number, visibleIndex, visibleResourceData } = this.props;
+    const { homeWorld, species } = this.state;
+    return (
+      <div>
+        <h1>{visibleResourceData[0][visibleIndex].name}</h1>
+        <h1>{visibleResourceData[0][visibleIndex].height / 100}m tall</h1>
+        <h1>{visibleResourceData[0][visibleIndex].mass}kg</h1>
+        <h1>Hair {visibleResourceData[0][visibleIndex].hair_color}</h1>
+        <h1>{visibleResourceData[0][visibleIndex].skin_color} skin</h1>
+        <h1>Born {visibleResourceData[0][visibleIndex].birth_year}</h1>
+        <h1>Gender {visibleResourceData[0][visibleIndex].gender}</h1>
+        <h4>{number}</h4>
+        <h4>Homeworld {homeWorld}</h4>
+        <h4>{species}</h4>
+      </div>
+    );
+  }
+}
 export default People;
