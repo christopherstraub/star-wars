bc19757cd37b40bc9e9bdd8b9fa3ccd0f887dc0b
christopherstraub <ccstraub@gmail.com>
2020-04-10 18:22:55 -0500

    changed background image on title page

--- src/components/TitlePage/TitlePage.js
+++ src/components/TitlePage/TitlePage.js
@@ -4,7 +4,7 @@ import './TitlePage.scss';
 
 const TitlePage = () => {
   return (
-    <div className="title-container vh-100 tc">
+    <div className="title-container vh-100 tc title-star-wars-font-italic-outline yellow">
       <div className="title dib">
         Star Wars
         <br />
@@ -15,7 +15,7 @@ const TitlePage = () => {
         <br />
         Explorer
       </div>
-      <img src={require('../../images/c-3po.png')} alt="C-3PO" />
+      <img src={require('../../images/darth-vader.png')} alt="Darth Vader" />
     </div>
   );
 };
