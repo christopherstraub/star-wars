2c4c57ad435df2dd7d1054038e84d97c00096bec
christopherstraub <ccstraub@gmail.com>
2020-04-11 12:37:28 -0500

    set background radial-gradient

--- src/wrappers/BackgroundAnimation/BackgroundAnimation.js
+++ src/wrappers/BackgroundAnimation/BackgroundAnimation.js
@@ -3,7 +3,7 @@ import './star-animation.css';
 
 const BackgroundAnimation = (props) => {
   return (
-    <div className="relative">
+    <div>
       <div id="stars"></div>
       <div id="stars2"></div>
       <div id="stars3"></div>
