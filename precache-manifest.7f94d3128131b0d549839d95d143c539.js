47fed4b145b7efbcd9142ef2ccff0dbe48d63976
christopherstraub <ccstraub@gmail.com>
2020-04-10 21:30:41 -0500

    Updates

---
+++ precache-manifest.7f94d3128131b0d549839d95d143c539.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2556211f67239fae186dfd87f223fdc",
+    "url": "/star-wars-explorer/index.html"
+  },
+  {
+    "revision": "bd9e07d1127652e704fa",
+    "url": "/star-wars-explorer/static/css/2.b4d5a309.chunk.css"
+  },
+  {
+    "revision": "ff1681a851634f40498e",
+    "url": "/star-wars-explorer/static/css/main.ec59d2a1.chunk.css"
+  },
+  {
+    "revision": "bd9e07d1127652e704fa",
+    "url": "/star-wars-explorer/static/js/2.5300b909.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-wars-explorer/static/js/2.5300b909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1681a851634f40498e",
+    "url": "/star-wars-explorer/static/js/main.9b46bdf8.chunk.js"
+  },
+  {
+    "revision": "707c99895a3bc54b7e36",
+    "url": "/star-wars-explorer/static/js/runtime-main.46a1207b.js"
+  },
+  {
+    "revision": "7c490d3004c1dab554bcf3990d43ddd7",
+    "url": "/star-wars-explorer/static/media/admiral-ackbar.7c490d30.svg"
+  },
+  {
+    "revision": "2f2b2e27d24ca6c9b4dc05dcf458b8ff",
+    "url": "/star-wars-explorer/static/media/boba-fett.2f2b2e27.svg"
+  },
+  {
+    "revision": "b583f44be95ea1429b82c726736000fd",
+    "url": "/star-wars-explorer/static/media/c3p0.b583f44b.svg"
+  },
+  {
+    "revision": "92969fb77a53ed9a1f397753d6115ef5",
+    "url": "/star-wars-explorer/static/media/chewbacca.92969fb7.svg"
+  },
+  {
+    "revision": "8dd121d2795843ef0f73eec30ae912c9",
+    "url": "/star-wars-explorer/static/media/clone-trooper.8dd121d2.svg"
+  },
+  {
+    "revision": "e5c6747c5381c346313e942095cf01b4",
+    "url": "/star-wars-explorer/static/media/darth-maul.e5c6747c.svg"
+  },
+  {
+    "revision": "2b2c3fb8778f73b053049ba899dceeb1",
+    "url": "/star-wars-explorer/static/media/darth-vader.2b2c3fb8.png"
+  },
+  {
+    "revision": "62fd30a9ec915b8d79cd2cc7ab3686b6",
+    "url": "/star-wars-explorer/static/media/darth-vader.62fd30a9.svg"
+  },
+  {
+    "revision": "4d0bec896758d62d43ac4db7312540f1",
+    "url": "/star-wars-explorer/static/media/emperor-palpatine.4d0bec89.svg"
+  },
+  {
+    "revision": "7783a166d2770ac6485c257495694f51",
+    "url": "/star-wars-explorer/static/media/ewok.7783a166.svg"
+  },
+  {
+    "revision": "2d159419ec2ee0ed7a6fa470dd7fba92",
+    "url": "/star-wars-explorer/static/media/gotu-regular.2d159419.ttf"
+  },
+  {
+    "revision": "c91254bcdb528ee5b58d3e4c2d21b6be",
+    "url": "/star-wars-explorer/static/media/greedo.c91254bc.svg"
+  },
+  {
+    "revision": "443c155f3df1a0dcef27a93f89952438",
+    "url": "/star-wars-explorer/static/media/han-solo.443c155f.svg"
+  },
+  {
+    "revision": "c5fb001d564ddf3f55886c99cb4a8c60",
+    "url": "/star-wars-explorer/static/media/heading-star-wars-font.c5fb001d.ttf"
+  },
+  {
+    "revision": "54bd5058999aefe56ff967466119941b",
+    "url": "/star-wars-explorer/static/media/jabba-the-hutt.54bd5058.svg"
+  },
+  {
+    "revision": "a06f3b71b57d8b51673fce6094ae2383",
+    "url": "/star-wars-explorer/static/media/jango-fett.a06f3b71.svg"
+  },
+  {
+    "revision": "08dbce77a08a11f26be31a1db25cd356",
+    "url": "/star-wars-explorer/static/media/jawa.08dbce77.svg"
+  },
+  {
+    "revision": "994cd2eb9b09746a0232d57477e3c723",
+    "url": "/star-wars-explorer/static/media/lando-calrissian.994cd2eb.svg"
+  },
+  {
+    "revision": "3f170e84b7a5cf439d0a195c0544a31b",
+    "url": "/star-wars-explorer/static/media/luke-skywalker.3f170e84.svg"
+  },
+  {
+    "revision": "ecaabf769674ef1144feef1e700f0a4a",
+    "url": "/star-wars-explorer/static/media/obiwan-kenobi.ecaabf76.svg"
+  },
+  {
+    "revision": "356346ce904846d73f32669be5ad7060",
+    "url": "/star-wars-explorer/static/media/princess-leia.356346ce.svg"
+  },
+  {
+    "revision": "80a24ae88a36d9a59f3b3bd787f172a6",
+    "url": "/star-wars-explorer/static/media/qui-gon-jinn.80a24ae8.svg"
+  },
+  {
+    "revision": "faa1256dde7920fd751fe066eee7cd6b",
+    "url": "/star-wars-explorer/static/media/r2d2.faa1256d.svg"
+  },
+  {
+    "revision": "939e1f120dd85f7ea594a1b308784d1d",
+    "url": "/star-wars-explorer/static/media/red-five.939e1f12.svg"
+  },
+  {
+    "revision": "76b51338974853d86250dab5307b2190",
+    "url": "/star-wars-explorer/static/media/stormtrooper.76b51338.svg"
+  },
+  {
+    "revision": "c1d313b09705a994232b09de87780555",
+    "url": "/star-wars-explorer/static/media/title-star-wars-font-bold.c1d313b0.ttf"
+  },
+  {
+    "revision": "1955fa3dbf39803ab301d61fe3db2a5f",
+    "url": "/star-wars-explorer/static/media/title-star-wars-font-italic-outline.1955fa3d.ttf"
+  },
+  {
+    "revision": "b738872d628bcd87877156696ce77979",
+    "url": "/star-wars-explorer/static/media/tusken-raider.b738872d.svg"
+  },
+  {
+    "revision": "8ba1cf66f75a31fdd4e70d0d4790c205",
+    "url": "/star-wars-explorer/static/media/yoda.8ba1cf66.svg"
+  }
+]);
